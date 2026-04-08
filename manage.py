4bbaac4547da78f57af9f2844c7696d016a28343
Coving Tron <255591504+covingtron@users.noreply.github.com>
2026-04-08 07:37:38 -0400

    Format and unique port

--- manage.py
+++ manage.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python
 """Django's command-line utility for administrative tasks."""
 
 import os
@@ -10,6 +9,10 @@ def main():
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
     try:
         from django.core.management import execute_from_command_line
+        from django.core.management.commands.runserver import Command
+        
+        Command.default_addr = '0.0.0.0'  # noqa: S104
+        Command.default_port = '7676'  # lynch library
     except ImportError as exc:
         raise ImportError(
             "Couldn't import Django. Are you sure it's installed and "
